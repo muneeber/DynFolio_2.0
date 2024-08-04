57707f81ef1fb8c8e12609386f8bcd02e3ada4be
Hafyz <hi@hafyz.dev>
2024-08-04 11:56:25 +0500

    mini projects done

--- app/Livewire/MiniProjects.php
+++ app/Livewire/MiniProjects.php
@@ -2,10 +2,16 @@
 
 namespace App\Livewire;
 
+use App\Models\MiniProject;
 use Livewire\Component;
 
 class MiniProjects extends Component
 {
+    public $minis;
+    public function mount(){
+    $this->minis=MiniProject::all();
+  
+    }
     public function render()
     {
         return view('livewire.mini-projects');

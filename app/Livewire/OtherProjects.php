5d834b16df3c2f56b6b086f92cad4668ca29accc
Hafyz <hi@hafyz.dev>
2024-08-04 12:10:10 +0500

    other projects section done

--- app/Livewire/OtherProjects.php
+++ app/Livewire/OtherProjects.php
@@ -3,9 +3,15 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\ProjectCategory;
 
 class OtherProjects extends Component
 {
+    public $projects;
+    public function mount(){
+    $this->projects=ProjectCategory::with('projects')->where('name','!=','Main')->get();
+    // dd($this->projects[0]->projects);
+    }
     public function render()
     {
         return view('livewire.other-projects');
--- resources/views/livewire/other-projects.blade.php
+++ resources/views/livewire/other-projects.blade.php
@@ -1,19 +1,25 @@
 <section class="grid gap-5 p-5 md:p-0">
-    <div class="text-xl font-bold text-highlight">Web3</div>
-    <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl::grid-cols-5 gap-5">
-        <div class="flex flex-col gap-3"><a target="_blank" href="/">
+    @foreach ($projects as $project)
+        
+    <div class="text-xl font-bold text-highlight">{{ $project->name }}</div>
+    <div class="grid text-white grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl::grid-cols-5 gap-5">
+        
+        @foreach ($project->projects as $item)
+            
+        <div class="flex flex-col gap-3">
+            <a target="_blank" href="/">
                 <div class="relative aspect-video rounded-lg overflow-hidden"><img alt="Solidity"
                         loading="lazy" decoding="async" data-nimg="fill" class="object-cover"
                         style="position:absolute;height:100%;width:100%;left:0;top:0;right:0;bottom:0;color:transparent"
                         sizes="100vw"
-                        src="https://ez-snippet.vercel.app/_next/image?url=%2Ftech%2Fweb3%2Fsolidity.jfif&w=3840&q=75">
+                        src="{{ Storage::url($item->image) }}">
                     <div class="progress-bar">
                         <div class="h-full bg-btnHighlight" style="width:90%"></div>
                     </div>
                 </div>
             </a>
             <div class="grid gap-2 px-2">
-                <div class="text-highlight font-bold">Solidity</div>
+                <div class="text-highlight font-bold">{{ $item->name }}</div>
                 <div class="flex gap-2 md:gap-10"><a target="_blank"
                         class="flex items-center text-info text-xs gap-1" href="/"><svg
                             xmlns="http://www.w3.org/2000/svg" width="16" height="16"
@@ -31,141 +37,12 @@
                             <path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8"></path>
                             <path d="M3 3v5h5"></path>
                             <path d="M12 7v5l4 2"></path>
-                        </svg>1 day ago</a></div>
+                        </svg>{{ $project->created_at->diffForHumans() }}</a></div>
             </div>
         </div>
-        <div class="flex flex-col gap-3"><a target="_blank" href="/">
-                <div class="relative aspect-video rounded-lg overflow-hidden"><img alt="Hardhat"
-                        loading="lazy" decoding="async" data-nimg="fill" class="object-cover"
-                        style="position:absolute;height:100%;width:100%;left:0;top:0;right:0;bottom:0;color:transparent"
-                        sizes="100vw"
-                        src="https://ez-snippet.vercel.app/_next/image?url=%2Ftech%2Fweb3%2Fhardhat.jpeg&w=3840&q=75">
-                    <div class="progress-bar">
-                        <div class="h-full bg-btnHighlight" style="width:80%"></div>
-                    </div>
-                </div>
-            </a>
-            <div class="grid gap-2 px-2">
-                <div class="text-highlight font-bold">Hardhat</div>
-                <div class="flex gap-2 md:gap-10"><a target="_blank"
-                        class="flex items-center text-info text-xs gap-1" href="/"><svg
-                            xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                            stroke-linecap="round" stroke-linejoin="round"
-                            class="lucide lucide-code">
-                            <polyline points="16 18 22 12 16 6"></polyline>
-                            <polyline points="8 6 2 12 8 18"></polyline>
-                        </svg>1,128</a><a target="_blank"
-                        class="flex items-center text-info text-xs gap-1" href="/"><svg
-                            xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                            stroke-linecap="round" stroke-linejoin="round"
-                            class="lucide lucide-history">
-                            <path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8"></path>
-                            <path d="M3 3v5h5"></path>
-                            <path d="M12 7v5l4 2"></path>
-                        </svg>1 day ago</a></div>
-            </div>
-        </div>
-        <div class="flex flex-col gap-3"><a target="_blank" href="/">
-                <div class="relative aspect-video rounded-lg overflow-hidden"><img alt="Metamask"
-                        loading="lazy" decoding="async" data-nimg="fill" class="object-cover"
-                        style="position:absolute;height:100%;width:100%;left:0;top:0;right:0;bottom:0;color:transparent"
-                        sizes="100vw"
-                        src="https://ez-snippet.vercel.app/_next/image?url=%2Ftech%2Fweb3%2Fmetamask.webp&w=3840&q=75">
-                    <div class="progress-bar">
-                        <div class="h-full bg-btnHighlight" style="width:90%"></div>
-                    </div>
-                </div>
-            </a>
-            <div class="grid gap-2 px-2">
-                <div class="text-highlight font-bold">Metamask</div>
-                <div class="flex gap-2 md:gap-10"><a target="_blank"
-                        class="flex items-center text-info text-xs gap-1" href="/"><svg
-                            xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                            stroke-linecap="round" stroke-linejoin="round"
-                            class="lucide lucide-code">
-                            <polyline points="16 18 22 12 16 6"></polyline>
-                            <polyline points="8 6 2 12 8 18"></polyline>
-                        </svg>1,128</a><a target="_blank"
-                        class="flex items-center text-info text-xs gap-1" href="/"><svg
-                            xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                            stroke-linecap="round" stroke-linejoin="round"
-                            class="lucide lucide-history">
-                            <path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8"></path>
-                            <path d="M3 3v5h5"></path>
-                            <path d="M12 7v5l4 2"></path>
-                        </svg>1 day ago</a></div>
-            </div>
-        </div>
-        <div class="flex flex-col gap-3"><a target="_blank" href="/">
-                <div class="relative aspect-video rounded-lg overflow-hidden"><img alt="Chainlink"
-                        loading="lazy" decoding="async" data-nimg="fill" class="object-cover"
-                        style="position:absolute;height:100%;width:100%;left:0;top:0;right:0;bottom:0;color:transparent"
-                        sizes="100vw"
-                        src="https://ez-snippet.vercel.app/_next/image?url=%2Ftech%2Fweb3%2Fchainlink.png&w=3840&q=75">
-                    <div class="progress-bar">
-                        <div class="h-full bg-btnHighlight" style="width:80%"></div>
-                    </div>
-                </div>
-            </a>
-            <div class="grid gap-2 px-2">
-                <div class="text-highlight font-bold">Chainlink</div>
-                <div class="flex gap-2 md:gap-10"><a target="_blank"
-                        class="flex items-center text-info text-xs gap-1" href="/"><svg
-                            xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                            stroke-linecap="round" stroke-linejoin="round"
-                            class="lucide lucide-code">
-                            <polyline points="16 18 22 12 16 6"></polyline>
-                            <polyline points="8 6 2 12 8 18"></polyline>
-                        </svg>1,128</a><a target="_blank"
-                        class="flex items-center text-info text-xs gap-1" href="/"><svg
-                            xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                            stroke-linecap="round" stroke-linejoin="round"
-                            class="lucide lucide-history">
-                            <path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8"></path>
-                            <path d="M3 3v5h5"></path>
-                            <path d="M12 7v5l4 2"></path>
-                        </svg>1 day ago</a></div>
-            </div>
-        </div>
-        <div class="flex flex-col gap-3"><a target="_blank" href="/">
-                <div class="relative aspect-video rounded-lg overflow-hidden"><img
-                        alt="OpenZeppelin" loading="lazy" decoding="async" data-nimg="fill"
-                        class="object-cover"
-                        style="position:absolute;height:100%;width:100%;left:0;top:0;right:0;bottom:0;color:transparent"
-                        sizes="100vw"
-                       src="https://ez-snippet.vercel.app/_next/image?url=%2Ftech%2Fweb3%2Fopenzeppelin.png&w=3840&q=75">
-                    <div class="progress-bar">
-                        <div class="h-full bg-btnHighlight" style="width:70%"></div>
-                    </div>
-                </div>
-            </a>
-            <div class="grid gap-2 px-2">
-                <div class="text-highlight font-bold">OpenZeppelin</div>
-                <div class="flex gap-2 md:gap-10"><a target="_blank"
-                        class="flex items-center text-info text-xs gap-1" href="/"><svg
-                            xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                            stroke-linecap="round" stroke-linejoin="round"
-                            class="lucide lucide-code">
-                            <polyline points="16 18 22 12 16 6"></polyline>
-                            <polyline points="8 6 2 12 8 18"></polyline>
-                        </svg>1,128</a><a target="_blank"
-                        class="flex items-center text-info text-xs gap-1" href="/"><svg
-                            xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                            stroke-linecap="round" stroke-linejoin="round"
-                            class="lucide lucide-history">
-                            <path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8"></path>
-                            <path d="M3 3v5h5"></path>
-                            <path d="M12 7v5l4 2"></path>
-                        </svg>1 day ago</a></div>
-            </div>
-        </div>
+        @endforeach
+      
     </div>
+    @endforeach
+    
 </section>

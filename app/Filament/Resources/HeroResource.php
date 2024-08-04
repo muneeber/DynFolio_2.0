e8f9f34df8d9ee4ac41771795a80c2180deebc5c
Hafyz <hi@hafyz.dev>
2024-08-04 06:08:36 +0500

    hero resource done

---
+++ app/Filament/Resources/HeroResource.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\HeroResource\Pages;
+use App\Filament\Resources\HeroResource\RelationManagers;
+use App\Models\Hero;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class HeroResource extends Resource
+{
+    protected static ?string $model = Hero::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('key')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('value')
+                    ->required()
+                    ->columnSpanFull()
+                    ->helperText('For Icons in SVG Form Refer To https://lucide.dev/'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('key')
+                    ->searchable(),
+                   
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListHeroes::route('/'),
+            'create' => Pages\CreateHero::route('/create'),
+            'edit' => Pages\EditHero::route('/{record}/edit'),
+        ];
+    }
+}
